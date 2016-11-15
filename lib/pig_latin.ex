04686d0167682046c10a2faf4457ab2600feec30
Joshua Philips <jmichaelphilips@gmail.com>
2016-11-14 23:28:29 -0600

    Made a function that returns a vowel list and a function that checks if the first letter is a vowel

--- lib/pig_latin.ex
+++ lib/pig_latin.ex
@@ -1,3 +1,14 @@
 defmodule PigLatin do
-    
+
+    def vowel_list do
+        ["a", "e", "i", "o", "u"]
+    end
+ 
+    def is_vowel?(word) do
+        [first_letter | _tail] = String.split(word, "")
+        Enum.member?(vowel_list, String.downcase(first_letter))
+    end
+
+
+
 end
